db9df194b6eac927211c616a01ba480357f12e83
z80 <z80dev@mlnl.finance>
2022-07-04 21:24:51 -0400

    expand clojure example

--- src/titanoboa/core.clj
+++ src/titanoboa/core.clj
@@ -6,6 +6,10 @@
 
 
 (def boa (py/import-module "boa"))
+(def boautil (py/import-module "boautil"))
+
+(defn format_addr [t]
+  ($a boautil :format_addr (py/->python t)))
 
 (defn boa-patch [boa field value]
   (let [patch (-> (py.- boa :env)
@@ -13,22 +17,30 @@
                   (py.- :patch))]
     (py/set-attr! patch field value)))
 
-(boa-patch boa :timestamp START_TIME)
 
-(def t (boa/load "tests/ElectricEel.vy"))
+(def t ($a boa :load "tests/ElectricEel.vy"))
 
 ($a t :foo)
 
-(defn format_addr [t]
-  (let [t-bytes (.getBytes t "utf-8")
-        missing (- 20 (count t-bytes))
-        padding (byte-array missing)]
-    (concat padding t-bytes)))
-
-(def e (boa/load "tests/ERC20.vy" "yfi token" "YFI" 18 0))
+(def a (format_addr "z80"))
 
 (def BUNNY (format_addr "bunny"))
 (def MILKY (format_addr "milky"))
 (def DOGGIE (format_addr "doggie"))
 (def POOLPI (format_addr "poolpi"))
 (def START_TIME 1641013200)
+(def DAY 86400)
+(def WEEK (* DAY 7))
+(def YEAR (* DAY 365))
+(def MAX_LOCK_DURATION (* YEAR 4))
+
+(boa-patch boa :timestamp START_TIME)
+
+(-> boa
+    (py.- :env)
+    ($a :generate_address))
+
+(let [env (py.- boa :env)]
+  (py/set-attr! env :eoa BUNNY))
+
+(def YFI ($a boa :load "tests/ERC20.vy" "yfi token" "YFI" 18 0 :override_address (format_addr "YFI")))
